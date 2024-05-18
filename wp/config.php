fc810ced0cecc5a654aa4d0337233a46e176f77d
yw <165012754+laoxieYYds@users.noreply.github.com>
2024-05-18 22:03:03 +0800

    Add files via upload

---
+++ wp/config.php
@@ -0,0 +1,10 @@
+<?php
+/*数据库配置*/
+$dbconfig=array(
+	"host" => "localhost", //数据库服务器
+	"port" => 3306, //数据库端口
+	"user" => "yanwu", //数据库用户名
+	"pwd" => "cengji", //数据库密码
+	"dbname" => "yanwu" //数据库名
+);
+?>
